4045abf996ed501e96bdafcc9b3cd1815715c864
José Alves Feitosa Neto <netoofeitosa@gmail.com>
2024-05-30 15:17:17 -0300

    Configuração Speed Insights Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./routes";
 import { AuthProvider } from "./contexts/Auth/AuthProvider";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <AppRouter />
-      </BrowserRouter>
-    </AuthProvider>
+    <>
+      <AuthProvider>
+        <BrowserRouter>
+          <AppRouter />
+        </BrowserRouter>
+      </AuthProvider>
+      <SpeedInsights />
+    </>
   );
 }
 
