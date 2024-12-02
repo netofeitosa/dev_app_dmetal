6c85ab1bb99e2c868ea71316f4e35c08465016a6
netofeitosa <netoofeitosa@gmail.com>
2024-12-02 14:21:33 -0300

    ajustes gerais

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
-  base: "/",
+  base: "/dev_app_dmetal",
 });
